bb0786b6cdc105355d99fb9a7c7e8ce5db913903
dhruv-pant_inmobi <dhruv.pant@inmobi.com>
2025-10-04 13:57:10 +0530

    adding android cfg target in build script

--- build.rs
+++ build.rs
@@ -4,8 +4,8 @@ fn main() {
         // For macOS with Homebrew
         println!("cargo:rustc-link-search=/opt/homebrew/lib");
         println!("cargo:rustc-link-lib=espeak-ng");
-    } else if cfg!(target_os = "linux") {
-        // For Linux
+    } else if cfg!(any(target_os = "linux", target_os = "android")) {
+        // For Linux and Android
         println!("cargo:rustc-link-lib=espeak-ng");
     }
 }
